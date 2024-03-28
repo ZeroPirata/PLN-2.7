87710732df600228d9e9beb181cc40396d0d403f
Gabriel Souza <gabriel0gsbn@gmail.com>
2024-03-27 22:09:50 -0300

    Correção do HTML nas tabelas pra ser legivel Tentativa de melhorar as respostas dos algoritmos Adição de novas palavras no dicionario

--- src/processing.py
+++ src/processing.py
@@ -7,7 +7,7 @@ import unicodedata
 
 from spellchecker import SpellChecker
 from bs4 import BeautifulSoup
-from functools import wraps
+from functools import lru_cache, wraps
 from time import perf_counter
 
 
@@ -18,7 +18,7 @@ def timing(func):
         result = func(*args, **kwargs)
         end_time = perf_counter()
         elapsed_time_ms = round(((end_time - start_time) * 1000), 2)
-        print(f"Função executada com: {elapsed_time_ms}")
+        print(f"A função '{func.__name__}' foi executada em: {elapsed_time_ms} ms")
         return result, elapsed_time_ms
 
     return wrapper
@@ -51,14 +51,12 @@ class Processing:
 
     @timing
     def __remocao_ruido(self):
-        """Remoção da parte do HTML e deixando apenas a sessão que contem o comentario dos usuarios dentro da página"""
+        """Remoção da parte do HTML e deixando apenas a sessão que contem o comentário dos usuários dentro da página"""
         soup = BeautifulSoup(self._raspagem[0], "html.parser")
         [s.extract() for s in soup(["iframe", "script"])]
         session = soup.find(self._tag, self._section)
         stripped_text = session.get_text()
-        stripped_text = re.sub(r"\s+", " ", stripped_text)
-        stripped_text = stripped_text.replace("\n", " ")
-        return stripped_text.strip()
+        return stripped_text
 
     @timing
     def __tokenizacao_por_frases(self):
@@ -82,7 +80,7 @@ class Processing:
     def __expansao_palavras(self):
         """Função para expandir palavras e corrigir acentuação"""
         expansion_dict: dict = self.load_expansion_dict(self._expansion_dict_file)
-        all_words = self._tokenizacao_palavras[0]
+        all_words = [word[:] for word in self._tokenizacao_palavras[0] if word]
         expanded_tokenizacao_palavras = []
 
         for words in all_words:
@@ -100,16 +98,18 @@ class Processing:
         portuguese = SpellChecker(language="pt")
         all_words = self._expansao_palavras[0]
         correcao_palavra_list = []
+
+        @lru_cache(maxsize=None)
+        def correcao_palavra(word):
+            correction_word = portuguese.correction(word)
+            return correction_word if correction_word is not None else word
+
         for words in all_words:
-            correcao_lista = []
-            for word in words:
-                correction_word = portuguese.correction(word)
-                correcao_lista.append(
-                    correction_word if correction_word is not None else word
-                )
+            correcao_lista = [correcao_palavra(word) for word in words]
             correcao_palavra_list.append(correcao_lista)
 
         return correcao_palavra_list
+
 
     @staticmethod
     def load_expansion_dict(expansion_dict_file):
@@ -134,7 +134,7 @@ class Processing:
             .decode("utf-8", "ignore")
         )
         special_chars_regex = r"[^a-zA-Z\s]"
-        text_cleaned = re.sub(special_chars_regex, "", text)
+        text_cleaned = re.sub(special_chars_regex, " ", text)
 
         return text_cleaned
 
